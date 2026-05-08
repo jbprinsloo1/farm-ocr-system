6f208cacc73aba06dbd3f9ea679eb1603bc6f8d2
Barend Prinsloo <jbprinsloo1@gmail.com>
2026-05-08 14:43:55 +0000

    Fix saveToSheet import

--- src/routes/scan.js
+++ src/routes/scan.js
@@ -3,7 +3,7 @@ const multer = require('multer');
 const path = require('path');
 const visionService = require('../services/vision');
 const parseVragData = require('../services/parser');
-const saveToSheet = require('../services/sheets');
+const { saveToSheet } = require('../services/sheets');
 
 const router = express.Router();
 
